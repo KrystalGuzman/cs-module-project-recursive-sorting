491bb572460fef2488ae7928831345a20ae6b654
Krystal Guzman <kryguzman@gmail.com>
2020-06-16 12:36:41 -0700

    finishes seearching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,24 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    # base condition not in array
+    if start > end:
+        return -1
+    # find middle value in search space
+    # and compare to key value
+    mid = (start + end) // 2
+    # base condition (key value found)
+    if target == arr[mid]:
+        return mid
+    # if in lower half
+    elif target < arr[mid]:
+        return binary_search(arr,target,start,mid-1)
+    # if in upper half
+    elif target > arr[mid]:
+        return binary_search(arr,target,mid+1,end)
+        
 
+# print(binary_search([1,3,4,6,8],3,1,8))
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
@@ -11,4 +28,4 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-
+    pass
